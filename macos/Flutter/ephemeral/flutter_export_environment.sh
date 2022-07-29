26d6286058becda7878339c56f8f4445c65845ec
Mahmoud Alaa <mahmoudadmob@gmail.com>
2022-07-29 10:28:27 +0200

    fix the project

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/java/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/java/Documents/Dev/GitHub/GithubActionsEvent"
+export "FLUTTER_APPLICATION_PATH=/Users/java/Documents/Dev/GitHub/GithubActionsEvent/hello"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
